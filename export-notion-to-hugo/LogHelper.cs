3d72f46533c1d5b5ecf9d72efa035421fb32f7e4
kapusch <BAILLATJean-Emmanuel@hotmail.com>
2022-11-01 18:39:14 +0100

    Setup initial logic for getting command line parameters

---
+++ export-notion-to-hugo/LogHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+
+public static class LogHelper
+{
+    const string SERAPATOR_LINE_1 = "..::..::..::..::..::..::..::..::..::..::..";
+    const string SERAPATOR_LINE_2 = "..........................................";
+
+    public static void PrintStart()
+    {
+        Console.WriteLine(SERAPATOR_LINE_1);
+        Console.WriteLine(FormatMessage("EXPORT NOTION TO HUGO", SERAPATOR_LINE_2));
+        Console.WriteLine(FormatMessage(". . . . . START . . . . .", SERAPATOR_LINE_2));
+        Console.WriteLine(SERAPATOR_LINE_1);
+    }
+
+    public static void PrintError(string errorMessage)
+    {
+        Console.WriteLine();
+        Console.WriteLine(FormatMessage(". . . . . ERROR . . . . .", SERAPATOR_LINE_2));
+        Console.WriteLine(FormatMessage(errorMessage, SERAPATOR_LINE_2));
+        Console.WriteLine(SERAPATOR_LINE_2);
+    }
+
+    public static void PrintMessage(string message)
+    {
+        Console.WriteLine();
+        Console.WriteLine(FormatMessage(message, SERAPATOR_LINE_2));
+    }
+
+    public static void PrintEnd()
+    {
+        Console.WriteLine();
+        Console.WriteLine(SERAPATOR_LINE_1);
+        Console.WriteLine(FormatMessage(". . . . . SUCCESS . . . . .", SERAPATOR_LINE_2));
+        Console.WriteLine(SERAPATOR_LINE_1);
+    }
+
+    static string FormatMessage(string message, string separator = SERAPATOR_LINE_1)
+    {
+        if(message.Length > separator.Length)
+        {
+            message = message.Substring(0, separator.Length - 5);
+            message += "[...]";
+        }
+
+        StringBuilder formattedMessage = new StringBuilder(separator);
+
+        int patternLength = (separator.Length - message.Length) / 2;
+
+        for (int i = 0; i < message.Length; i++)
+        {
+            formattedMessage[i + patternLength] = message[i];
+        }
+
+        return formattedMessage.ToString();
+    }
+}
---
+++ export-notion-to-hugo/Program.cs
@@ -0,0 +1,53 @@
+﻿LogHelper.PrintStart();
+
+// dotnet run "Username=user@email.com" "Password=1234" "DatabaseID=12345abcde" "Status=published"
+Parameters parameters = new();
+var parameterNames = (parameters.GetType()).GetProperties().Select(q => q.Name);
+
+if (args.Length != 4)
+{
+    LogHelper.PrintError($"{args.Length} found arguments instead of 4");
+    Exit();
+}
+
+foreach (var argument in args)
+{
+    if (!parameterNames.Any(parameterName => argument.StartsWith(parameterName)))
+    {
+        LogHelper.PrintError($"Unexpected argument: {argument}");
+        Exit();
+    }
+    else
+    {
+        var parameterName = parameterNames.First(parameterName => argument.StartsWith(parameterName));
+        if (argument.IndexOf('=') == -1)
+        {
+            LogHelper.PrintError($"Cannot parse value for argument: {parameterName}");
+            Exit();
+        }
+        else
+        {
+            var argumentValue = argument.Substring(argument.IndexOf('=') + 1);
+            (parameters.GetType())
+                .GetProperty(parameterName)
+                .SetValue(parameters, argumentValue);
+        }
+    }
+}
+
+LogHelper.PrintEnd();
+Exit();
+
+void Exit()
+{
+    Console.ReadKey(true);
+    Environment.Exit(0);
+}
+
+class Parameters
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string DatabaseID { get; set; }
+    public string Status { get; set; }
+}
