c0bbef5018194497c8879e6063ec6a581e4b07a8
kapusch <BAILLATJean-Emmanuel@hotmail.com>
2023-04-09 18:13:02 +0200

    Fix uncommented line for debug purpose

--- export-notion-to-hugo/Program.cs
+++ export-notion-to-hugo/Program.cs
@@ -16,7 +16,7 @@ try
 
     List<Page> test = new List<Page>();
     // NOTE: Uncomment for debugging a specific page export
-    test.Add(await notionAPI.GetPageById("753688cef4674473b8380b7f56a19827"));
+    //test.Add(await notionAPI.GetPageById("753688cef4674473b8380b7f56a19827"));
 
     var pagesRetrievedFromNotion = await notionAPI.GetPagesFromDatabase(parameters.DatabaseId, parameters.Status);
     foreach (var page in test.Count == 0 ? pagesRetrievedFromNotion.Results : test)
